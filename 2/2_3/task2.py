c3a5a57cb1abdbefe79ada8e6b19024bb68281a4
AA22_Gi <gibert22@mail.ru>
2024-12-07 23:58:35 +0700

    .

--- 2/2_3/task2.py
+++ 2/2_3/task2.py
@@ -3,4 +3,16 @@ class Config:
 
 
 def create_instance(n: int) -> Config:
-    pass
+    obj = Config()
+    for i in range(1, n + 1):
+        setattr(obj, f'attribute{i}', f'value{i}')
+    return obj
+
+
+if __name__ == '__main__':
+    config_4 = create_instance(4)
+    config_3 = create_instance(3)
+    config_2 = create_instance(2)
+    print(config_4.__dict__)
+    print(config_3.__dict__)
+    print(config_2.__dict__)
