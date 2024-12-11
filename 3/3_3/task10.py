1e643bf7b948aab82d11e3f6b38a8d6910f31871
AA22_Gi <gibert22@mail.ru>
2024-12-11 14:58:58 +0700

    Переписан метод set_coordinates на __init__

--- 3/3_3/task10.py
+++ 3/3_3/task10.py
@@ -19,7 +19,7 @@ from math import sqrt
 
 
 class Point:
-    def set_coordinates(self, x, y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
@@ -38,3 +38,10 @@ class Point:
 
     def get_point_with_max_distance(self):
         pass
+
+
+if __name__ == '__main__':
+    p1 = Point(4, 5)
+    p2 = Point(2, 4)
+    p3 = Point(5, 1)
+    p2.get_point_with_max_distance()
