9d3898ae456ac3d7f4507ab70710b9b3846fc7f5
AA22_Gi <gibert22@mail.ru>
2024-12-12 20:12:33 +0700

    Создан класс BankAccount В нем реализованы методы __init__, get_account_number, get_balance, set_balance

--- 3/3_6/task1.py
+++ 3/3_6/task1.py
@@ -6,3 +6,19 @@
     3) геттер-метод для атрибута _balance
     4) сеттер-метод для атрибута _balance
 """
+
+
+class BankAccount:
+    def __init__(self, account_number, balance):
+        self._account_number = account_number
+        self._balance = balance
+
+    def get_account_number(self):
+        return self._account_number
+
+    def get_balance(self):
+        return self._balance
+
+    def set_balance(self, new_balance):
+        self._balance = new_balance
+
