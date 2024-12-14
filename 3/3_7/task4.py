7a1a7447e97645848e9eb42b8682ac08c6d6664d
AA22_Gi <gibert22@mail.ru>
2024-12-14 22:46:11 +0700

    написано свойство-геттер dollars

--- 3/3_7/task4.py
+++ 3/3_7/task4.py
@@ -30,4 +30,6 @@ class Money:
     def __init__(self, dollars, cents):
         self.total_cents = dollars * 100 + cents
 
-
+    @property
+    def dollars(self):
+        return self.total_cents / 100
