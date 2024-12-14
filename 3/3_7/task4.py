92fe1917fc204788d74d699bea5dee147dae5f5d
AA22_Gi <gibert22@mail.ru>
2024-12-14 23:14:43 +0700

    из метода __init__ удалены атрибуты dollars, cents

--- 3/3_7/task4.py
+++ 3/3_7/task4.py
@@ -28,13 +28,11 @@
 
 class Money:
     def __init__(self, dollars, cents):
-        self.dollars = dollars
-        self.cents = cents
-        self.total_cents = self.dollars * 100 + self.cents
+        self.total_cents = dollars * 100 + cents
 
     @property
     def dollars(self):
-        return self.total_cents / 100
+        return self.total_cents // 100
 
     @dollars.setter
     def dollars(self, new_dollars):
@@ -45,7 +43,7 @@ class Money:
 
     @property
     def cents(self):
-        return self.cents
+        return self.total_cents % 100
 
     @cents.setter
     def cents(self, new_cents):
@@ -58,3 +56,28 @@ class Money:
         return f'Ваше состояние составляет {self.dollars} долларов {self.cents} центов'
 
 
+if __name__ == '__main__':
+    pass
+    bill = Money(101, 99)
+    assert isinstance(bill, Money)
+
+    print(bill)  # Ваше состояние составляет 101 долларов 99 центов
+    print(bill.dollars, bill.cents)  # 101 99
+    bill.dollars = 666
+    print(bill)  # Ваше состояние составляет 666 долларов 99 центов
+    bill.cents = 12
+    print(bill)  # Ваше состояние составляет 666 долларов 12 центов
+    assert bill.total_cents == 66612
+    assert list(bill.__dict__.keys()) == ['total_cents']
+    print()
+
+    ken = Money(111, 90)
+    assert isinstance(ken, Money)
+    print(ken)
+    ken.dollars = 'hello'  # Error dollars
+    ken.dollars = 0
+    print(ken)
+    ken.cents = [1, 2, 3]  # Error cents
+    ken.cents = 100  # Error cents
+    ken.cents = 99
+    print(ken)
