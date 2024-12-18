578680dd617a622cfab49f11d707b1c8b0ab6a1e
AA22_Gi <gibert22@mail.ru>
2024-12-18 07:07:40 +0700

    написано условие задачи

---
+++ 3/3_9/task4.py
@@ -0,0 +1,33 @@
+"""
+Перед вами имеется реализация класса Circle. Ваша задача добавить в него следующее:
+
+    1)  класс-метод from_diameter, принимающий диаметр круга.
+        Метод from_diameter должен возвращать новый экземпляр класса Circle
+        (учитывайте, что экземпляры круга создаются по радиусу);
+
+    2)  статик-метод is_positive, принимающий одно число.
+        Метод is_positive должен возвращать ответ, является ли переданное число положительным;
+
+    3)  статик-метод area, который принимает радиус и возвращает площадь круга.
+        Для этого воспользуйтесь формулой pi∗r и в качестве значения pi возьмите 3.14
+
+Ваша задача только добавить реализацию трех методов в класс Circle
+"""
+
+
+class Circle:
+
+    def __init__(self, radius):
+        if not Circle.is_positive(radius):
+            raise ValueError("Радиус должен быть положительным")
+        self.radius = radius
+
+# код ниже не нужно удалять, в нем находятся проверки
+circle_1 = Circle.from_diameter(10)
+assert isinstance(circle_1, Circle)
+assert circle_1.radius == 5.0
+print(f"circle_1.radius={circle_1.radius}")
+assert Circle.is_positive(10)
+assert not Circle.is_positive(-5)
+assert Circle.area(1) == 3.14
+assert Circle.area(2) == 12.56
