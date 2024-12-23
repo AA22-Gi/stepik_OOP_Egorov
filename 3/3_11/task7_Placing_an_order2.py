5399d013374a33d05a71f25e52147cd810f6e06c
AA22_Gi <gibert22@mail.ru>
2024-12-23 12:55:57 +0700

    .

--- 3/3_11/task7_Placing_an_order2.py
+++ 3/3_11/task7_Placing_an_order2.py
@@ -1,43 +1,35 @@
 """
-Задача «Оформление заказа» Часть 3
+Задача «Оформление заказа» Часть 2
 
-Последний штрих — создание класса корзины, куда пользователь будет складывать товары.
-Для этого нам понадобятся ранее созданные классы User и Product
+Далее для оформления заказа нам нужен пользователь. Для этого создадим класс User, который содержит:
 
-Итак, создаем класс Cart, который содержит:
+    1)  метод __init__, принимающий на вход логин пользователя
+        и необязательный аргумент — баланс его счета (по умолчанию 0).
+        Логин необходимо сохранить в атрибуте login,
+        а баланс необходимо присвоить сеттеру balance (см. пункт 4)
 
-    1)  метод __init__, принимающий на вход экземпляр класса User.
-        Его необходимо сохранить в атрибуте user.
-        Помимо этого, метод  должен создать атрибут goods — пустой словарь (лучше использовать defaultdict).
-        Он нужен будет для хранения товаров и их количества (ключ словаря — товар, значение — количество).
-        И еще нам понадобится создать защищенный атрибут .__total со значением 0.
-        В нем будет храниться итоговая сумма заказа
+    2)  метод __str__, возвращающий строку вида:
+            Пользователь {login}, баланс - {balance}
 
-    2)  метод add принимает на вход экземпляр класса Product
-        и необязательный аргумент — количество товаров (по умолчанию 1).
-        Метод add  должен увеличить в корзине (атрибут goods) количество указанного товара
-        на переданное значение количества и пересчитать атрибут self.__total
+    3)  Свойство геттер balance, которое возвращает значение self.__balance
 
-    3)  метод remove принимает на вход экземпляр класса Product
-        и необязательный аргумент — количество товаров (по умолчанию 1).
-        Метод remove  должен уменьшить в корзине (атрибут goods) количество указанного товара
-        на переданное значение количества и пересчитать атрибут self.__total.
-        Обратите внимание на то, что количество товара в корзине не может стать отрицательным, как и итоговая сумма;
+    4)  Свойство сеттер balance, принимает новое значение баланса и устанавливает его в атрибут self.__balance
 
-    4)  свойство геттер total, которое возвращает значение self.__total;
+    5)  метод deposit  принимает числовое значение и прибавляет его к атрибуту self.__balance
 
-    5)  метод order  должен использовать метод payment из класса User и передать в него итоговую сумму корзины.
-        В случае, если средств пользователю хватило, вывести сообщение «Заказ оплачен»,
-        в противном случае - «Проблема с оплатой»;
+    6)  метод is_money_enough, который принимает числовое значение и проверяет,
+        имеется ли у пользователя такая сумма на балансе.
+        Данный метод должен возвращать булево значение:
+        если такая сумма есть – True, если нет – False
 
-    6)  метод print_check, печатающий на экран чек. Он должен начинаться со строки
-        ---Your check---
-        Затем выводится состав корзины в алфавитном порядке по названию товара в виде
-        {Имя товара} {Цена товара} {Количество товара} {Сумма}
-        И заканчивается чек строкой
-        ---Total: {self.total}---
+    7)  метод payment  принимает числовое значение, которое должно списаться с баланса пользователя.
+        Если на счете у пользователя не хватает средств, то необходимо вывести фразу:
+            Не хватает средств на балансе. Пополните счет
+        и отказ в платеже. Если средств хватает, списываем с баланса у пользователя указанную сумму.
+        (Постарайтесь внутри реализации воспользоваться методом is_money_enough)
+
+Необходимо только написать определение класса User
 """
-from collections import defaultdict
 
 
 class Product:
@@ -75,49 +67,6 @@ class User:
             print(f'Не хватает средств на балансе. Пополните счет')
 
 
-class Cart:
-    def __init__(self, user: User):
-        self.user = user
-        self.goods = defaultdict(int)  # Словарь для хранения товаров и их количества
-        self.__total = 0  # Итоговая сумма заказа
-
-    def add(self, product: Product, count_goods: int = 1):
-        self.goods[product] += count_goods
-        self.__total += product.price * count_goods  # Увеличиваем итоговую сумму
-
-    def remove(self, product: Product, count_goods: int = 1):
-        if product in self.goods:
-            current_count = self.goods[product]
-            if current_count >= count_goods:
-                self.goods[product] -= count_goods
-                self.__total -= product.price * count_goods  # Уменьшаем итоговую сумму
-                if self.goods[product] == 0:  # Удаляем товар, если его количество стало 0
-                    del self.goods[product]
-            else:
-                del self.goods[product]
-                self.__total -= product.price * current_count  # Уменьшаем итоговую сумму на все оставшиеся товары
-
-    @property
-    def total(self):
-        return self.__total
-
-    def order(self):
-        if self.user.is_money_enough(self.__total):
-            self.user.payment(self.__total)
-            print("Заказ оплачен")
-        else:
-            print(f'Не хватает средств на балансе. Пополните счет')
-            print("Проблема с оплатой")
-
-    def print_check(self):
-        print('---Your check---')
-        for product in sorted(self.goods.keys(), key=lambda p: p.name):  # Сортируем по имени товара
-            quantity = self.goods[product]
-            total_price = product.price * quantity
-            print(f'{product.name} {product.price} {quantity} {total_price}')
-        print(f'---Total: {self.total}---')
-
-
 if __name__ == '__main__':
     billy = User('billy@rambler.ru')
     assert billy.__str__() == 'Пользователь billy@rambler.ru, баланс - 0'
@@ -151,47 +100,3 @@ if __name__ == '__main__':
     assert jack.payment(1) is None  # Проверяем, что платеж прошел
     assert jack.__str__() == 'Пользователь jack@gmail.ru, баланс - 0'
     print('Good')
-
-    billy = User('billy@rambler.ru')
-
-    lemon = Product('lemon', 20)
-    carrot = Product('carrot', 30)
-
-    cart_billy = Cart(billy)
-    print(cart_billy.user)  # Пользователь billy@rambler.ru, баланс - 0
-    cart_billy.add(lemon, 2)
-    cart_billy.add(carrot)
-    cart_billy.print_check()
-    ''' Печатает текст ниже
-    ---Your check---
-    carrot 30 1 30
-    lemon 20 2 40
-    ---Total: 70---'''
-    cart_billy.add(lemon, 3)
-    cart_billy.print_check()
-    ''' Печатает текст ниже
-    ---Your check---
-    carrot 30 1 30
-    lemon 20 5 100
-    ---Total: 130---'''
-    cart_billy.remove(lemon, 6)
-    cart_billy.print_check()
-    ''' Печатает текст ниже
-    ---Your check---
-    carrot 30 1 30
-    ---Total: 30---'''
-    print(cart_billy.total)  # 30
-    cart_billy.add(lemon, 5)
-    cart_billy.print_check()
-    ''' Печатает текст ниже
-    ---Your check---
-    carrot 30 1 30
-    lemon 20 5 100
-    ---Total: 130---'''
-    cart_billy.order()
-    ''' Печатает текст ниже
-    Не хватает средств на балансе. Пополните счет
-    Проблема с оплатой'''
-    cart_billy.user.deposit(150)
-    cart_billy.order()  # Заказ оплачен
-    print(cart_billy.user.balance)  # 20
